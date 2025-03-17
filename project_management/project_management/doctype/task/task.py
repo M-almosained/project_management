0f628aea693e97f6a9d4b65cdf8b74634ad9a85e
M-almosained <m.almosained@sanaam.sa>
2025-03-17 19:55:44 +0300

    on save

--- project_management/project_management/doctype/task/task.py
+++ project_management/project_management/doctype/task/task.py
@@ -1,9 +1,34 @@
 # Copyright (c) 2025, f and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Task(Document):
-	pass
+	def validate(self):
+		self.calculate_total_hours()
+
+	def calculate_total_hours(self):
+		"""Calculate total hours from TimeSheet entries and update hour_count"""
+		# Get total hours from timesheet
+		total_hours = frappe.get_value('TimeSheet', {'task': self.name, 'docstatus': 1}, 'sum(hours)') or 0
+		
+		# Update the hour_count field
+		self.hour_count = total_hours
+
+		# Update assignment table with total hours
+		if self.assignment:
+			for assignment in self.assignment:
+				# Get employee specific hours from timesheet
+				employee_hours = frappe.get_value('TimeSheet', 
+					{
+						'task': self.name, 
+						'employee': assignment.employee, 
+						'docstatus': 1
+					}, 
+					'sum(hours)'
+				) or 0
+				
+				# Update employee's hour count in the assignment table
+				assignment.hour_count = employee_hours
--- project_management/project_management/doctype/timesheet/timesheet.py
+++ project_management/project_management/doctype/timesheet/timesheet.py
@@ -1,9 +1,52 @@
 # Copyright (c) 2025, f and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class TimeSheet(Document):
-	pass
+	def validate(self):
+		self.validate_hours()
+		self.update_task_hours()
+
+	def validate_hours(self):
+		"""Validate that hours are not negative"""
+		if self.hours < 0:
+			frappe.throw("Hours cannot be negative")
+
+	def update_task_hours(self):
+		"""Update task hours when timesheet is submitted"""
+		if self.task and self.docstatus == 1:
+			# Get the task document
+			task = frappe.get_doc('Task', self.task)
+			
+			# Calculate total hours for this task
+			total_hours = frappe.get_value('TimeSheet', 
+				{'task': self.task, 'docstatus': 1}, 
+				'sum(hours)'
+			) or 0
+			
+			# Update task's hour_count
+			task.hour_count = total_hours
+			
+			# Update assignment table if exists
+			if task.assignment:
+				for assignment in task.assignment:
+					if assignment.employee == self.employee:
+						# Calculate employee specific hours
+						employee_hours = frappe.get_value('TimeSheet', 
+							{
+								'task': self.task,
+								'employee': self.employee,
+								'docstatus': 1
+							},
+							'sum(hours)'
+						) or 0
+						
+						# Update employee's hour count
+						assignment.hour_count = employee_hours
+						break
+			
+			# Save the task
+			task.save()
